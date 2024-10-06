4fcadc4c5c0d604f8e309ef5008f5e98e3b243c3
CHETAN B R <chiranjeevichetan1996@gmail.com>
2024-10-06 10:54:33 +0530

    Updated Uniform safety

--- configs/FirebaseConfig.js
+++ configs/FirebaseConfig.js
@@ -4,6 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,3 +21,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, { persistence: getReactNativePersistence(ReactNativeAsyncStorage) });
 export const db = getFirestore(app);
+export const storage = getStorage(app);
