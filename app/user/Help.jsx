ae642144834876bad5fc277e63ae6756dec31505
CHETAN B R <chiranjeevichetan1996@gmail.com>
2024-10-06 19:54:19 +0530

    Help.jsx

--- app/user/Help.jsx
+++ app/user/Help.jsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+// import { useRouter } from 'expo-router'
+import HelpCenter from './../userPages/helpcenter'
 
 export default function Help() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Help</Text>
+    <View>
+      <HelpCenter />
     </View>
   )
 }
