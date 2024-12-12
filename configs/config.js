a2b569f39041877bce322fbb832ef9f18f1b787c
CHETAN B R <chiranjeevichetan1996@gmail.com>
2024-12-12 07:46:27 +0530

    Update Google API key

--- configs/config.js
+++ configs/config.js
@@ -1,12 +1,12 @@
 const config = {
   development: {
     BASE_URL: "http://192.168.0.108:3000",
-    GEMINI_API_KEY: "AIzaSyBnq3yyUg63Eb3a-3jnBuVR6JT5D7zlaBY",
+    GEMINI_API_KEY: "AIzaSyByr0MMSAV70uJveZj6XAUbvKODOBhlulI",
     GOOGLE_CLOUD_API_KEY: "AIzaSyDKbV2_T0sxpjbsL_IidOc4PgCHUIBkgkc"
   },
   production: {
     BASE_URL: "https://incisafemanagerserver-eyfdfhgra3b8ggca.centralindia-01.azurewebsites.net",
-    GEMINI_API_KEY: "AIzaSyBnq3yyUg63Eb3a-3jnBuVR6JT5D7zlaBY",
+    GEMINI_API_KEY: "AIzaSyByr0MMSAV70uJveZj6XAUbvKODOBhlulI",
     GOOGLE_CLOUD_API_KEY: "AIzaSyDKbV2_T0sxpjbsL_IidOc4PgCHUIBkgkc"
   },
 };
